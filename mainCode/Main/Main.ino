2a9f5af016c3862408b9d51329b9680f6b71fa2d
Mauro <maurodebruyn@hotmail.com>
2024-03-19 16:06:11 +0100

    Add updated code + add display code

--- mainCode/Main/Main.ino
+++ mainCode/Main/Main.ino
@@ -1,7 +1,8 @@
 #include <RTClib.h>
 #include <OneWire.h>
 
-OneWire  ds(3);  // Connect 1-wire sensor to pin 3
+OneWire  ds(5);  // Connect 1-wire sensor to pin 3
+
 RTC_DS1307 rtc;
 
 char daysOfTheWeek[7][12] = {
@@ -34,16 +35,17 @@ void timeStamp(void){
   delay(1000); // delay 1 seconds
 }
 
-void logPerson(void) {
+void logIButton(void) {
   byte i;
   byte present = 0;
   byte data[12];
   byte addr[8];
 
-  //Serial.print("Looking for 1-Wire devices...\n\r");
+  //Serial.print("Present I-Button...\n\r");
   while(ds.search(addr)) {
     Serial.print("\n\n\n");
     Serial.print("\n\Address:\n\r");
+    analogWrite(8, 255);
     for( i = 0; i < 8; i++) {
       if (addr[i] < 16) {
         Serial.print('0');
@@ -55,13 +57,15 @@ void logPerson(void) {
     }
     Serial.print("\n\n");
     timeStamp();
+    
     //Serial.print("\n\n\n");
 
     if ( OneWire::crc8( addr, 7) != addr[7]) {
         Serial.print("CRC is not valid!\n");
         return;
     }
-    delay(2000); //scan time incraments
+    delay(1000); //scan time incraments
+    analogWrite(8, 0);
   }
   ds.reset_search();
   return;
@@ -86,9 +90,12 @@ void setup() {
   // rtc.adjust(DateTime(2021, 1, 21, 3, 0, 0));
 
   //IButtonRead
+
+  pinMode(8, OUTPUT); // Set the LED pin as an output
 }
 
 void loop() {
-  logPerson();
+  logIButton();
+
 
 }
