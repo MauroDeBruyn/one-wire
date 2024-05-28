3d695f24f3a39b0f273ce09e7517d79ea8515a16
Thibe <114075982+ThibeVanOrshaegen@users.noreply.github.com>
2024-05-28 17:41:58 +0200

    Update slaveCode.ino

--- mainCode/slaveCode/slaveCode.ino
+++ mainCode/slaveCode/slaveCode.ino
@@ -1,45 +1,117 @@
-#include <ArduinoJson.h>
+#include <Wire.h>
+#include <Arduino_LSM9DS1.h>
+#include <Arduino_HTS221.h>
 
-#define LED 2
+float x, y, z;
+int degreesX = 0;
+int degreesY = 0;
+
+float old_temp = 0;
+float old_hum = 0;
+
+#define SENSOR_ADDRESS 0x12 // Address of the sensor device
 
 void setup() {
-   Serial.begin(9600);
-   Serial2.begin(115200);
-   pinMode(LED, OUTPUT);
+  Wire.begin();
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("Started");
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+   if (!HTS.begin()) {
+    Serial.println("Failed to initialize humidity temperature sensor!");
+    while (1);
+  }
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println("Hz");
 }
 
 void loop() {
-  Serial2.write('1'); 
-  digitalWrite(LED, HIGH); 
-  StaticJsonDocument<200> doc;
 
-  delay(1000); 
 
-  if (Serial2.available() >= 1) {
-    char buffer[200];
-    Serial2.readBytesUntil('\n', buffer, 200);
+  float temperature = HTS.readTemperature(); //read
+  float humidity    = HTS.readHumidity(); //read
 
-    DeserializationError error = deserializeJson(doc, buffer);
-    if (error) {
-      Serial.print("deserializeJson() failed: ");
-      Serial.println(error.c_str());
-      return;
-    }
-
-    const int sensorId = doc["Id"];
-    const char* number = doc["Number"];
-    const int colornumber = doc["ColorNumber"];
-
-    Serial.print("Id: ");
-    Serial.println(sensorId);
-    Serial.print("Colornumber: ");
-    Serial.println(number);
-    Serial.print("ColorNumber: ");
-    Serial.println(colornumber);
+ if (abs(old_temp - temperature) >= 0.5 || abs(old_hum - humidity) >= 1 )
+  {
+    old_temp = temperature;
+    old_hum = humidity;
+    // print each of the sensor values
+    Serial.print("Temperature = ");
+    Serial.print(temperature);
+    Serial.println(" °C");
+    Serial.print("Humidity    = ");
+    Serial.print(humidity);
+    Serial.println(" %");
+    Serial.println();
   }
 
-  Serial2.write('0');
-  digitalWrite(LED, LOW);
+   Serial.print("Temperature = ");
+  Serial.print(temperature);
+  Serial.println(" °C");
 
-  delay(1000); 
+  Serial.print("Humidity    = ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  // print an empty line
+  Serial.println();
+
+  delay(1000);
+
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(x, y, z);
+
+  }
+
+  if (x > 0.1) {
+    x = 100 * x;
+    degreesX = map(x, 0, 97, 0, 90);
+    Serial.print("Tilting up ");
+    Serial.print(degreesX);
+    Serial.println("  degrees");
+  }
+  if (x < -0.1) {
+    x = 100 * x;
+    degreesX = map(x, 0, -100, 0, 90);
+    Serial.print("Tilting down ");
+    Serial.print(degreesX);
+    Serial.println("  degrees");
+  }
+  if (y > 0.1) {
+    y = 100 * y;
+    degreesY = map(y, 0, 97, 0, 90);
+    Serial.print("Tilting left ");
+    Serial.print(degreesY);
+    Serial.println("  degrees");
+  }
+  if (y < -0.1) {
+    y = 100 * y;
+    degreesY = map(y, 0, -100, 0, 90);
+    Serial.print("Tilting right ");
+    Serial.print(degreesY);
+    Serial.println("  degrees");
+  }
+
+    // Send the fixed number over I2C
+  Wire.beginTransmission(8); // Address of the Arduino Uno
+  Wire.write((uint8_t*)&temperature, sizeof(temperature));
+  Wire.write((uint8_t*)&humidity, sizeof(humidity));
+  Wire.write((uint8_t*)&x, sizeof(x));
+  Wire.write((uint8_t*)&y, sizeof(y));
+  Wire.write((uint8_t*)&z, sizeof(z));
+  Wire.write((uint8_t*)&degreesX, sizeof(degreesX));
+
+  
+  Wire.endTransmission();
+
+  Serial.println(temperature);
+  Serial.println(humidity);
+  Serial.println(degreesX);
+
+
 }
